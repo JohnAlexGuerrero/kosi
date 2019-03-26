ca7a01cf835052113ccb1d6c14b9ed03d03357b8
johnalexguerrero <johnalexguerrero@gmail.com>
2019-03-25 23:31:41 -0500

    conexion con db y creacion de primera table

---
+++ conexion_db_ferreteria.php
@@ -0,0 +1,10 @@
+<?php
+
+//clase para realizar la conexion con la base de datos
+    $servidor = "localhost";
+    $usuario = "root";
+    $password = "";
+    $base_datos = "db_ferreteria";
+
+    //Rrequiere("conexion_db_ferreteria.php");
+?>
---
+++ operaciones.php
@@ -0,0 +1,8 @@
+<?php
+    include "conexion_db_ferreteria";
+
+    $conexion = new conexion_db_ferreteria();
+
+    
+
+?>
